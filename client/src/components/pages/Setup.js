388ec70f8edfadf67b455903c40df9a6c76ece9b
Josh Brooks <defaultjosh@hotmail.com>
2020-07-11 13:33:43 -0400

    Added footer to all pages

--- client/src/components/pages/Setup.js
+++ client/src/components/pages/Setup.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Footer from "../Footer.js";
 
 export default function Setup() {
     return (
         <div>
-            
+            Hello Setup!
+            <Footer />
         </div>
     )
 }
