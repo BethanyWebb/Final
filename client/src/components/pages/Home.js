a5eb04670ced32d5efe244eab2889d18a760e3a0
Josh Brooks <defaultjosh@hotmail.com>
2020-07-13 14:17:26 -0400

    Set up basic landing page layout

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Footer from "../Footer.js";
+import "../styles/Home.css";
 
 export default function Home() {
     return (
         <div>
-            Hello Home!
+            <div className="basics">
+                <h1>Welcome to Chicken Tinder - Or header here instead</h1>
+                <h2>Description Here</h2>
+                <h3>Please log in or <a href="/signup">sign up</a></h3>
+                <form>
+                    <input type="email" placeholder="Email"></input><br />
+                    <input type="password" placeholder="Password"></input><br />
+                    <button>Log in</button>
+                </form>
+            </div>
             <Footer />
         </div>
     )
