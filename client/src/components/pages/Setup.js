c20cc699f61cc95e8905f495d1530b7b2505d1de
Josh Brooks <defaultjosh@hotmail.com>
2020-07-13 14:53:41 -0400

    Basic page layout made

--- client/src/components/pages/Setup.js
+++ client/src/components/pages/Setup.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import Footer from "../Footer.js";
+import "../styles/Setup.css";
 
 export default function Setup() {
     return (
         <div>
-            Hello Setup!
+            <div className="basics">
+                <h1>Welcome to Chicken Tinder</h1>
+                <h2>Please Create Your Profile</h2>
+                <form>
+                <input type="text" placeholder="Username"></input><br />
+                    <input type="email" placeholder="Email"></input><br />
+                    <input type="password" placeholder="Password"></input><br />
+                    <button>Submit</button>
+                </form>
+                <a href="/"><button>Home</button></a>
+            </div>
             <Footer />
         </div>
     )
