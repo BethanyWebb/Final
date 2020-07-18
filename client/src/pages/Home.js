0364c4cedd10fb780a5a0aa3080ba628d40d3bfa
Josh Brooks <defaultjosh@hotmail.com>
2020-07-18 13:50:51 -0400

    Pages now use state. Moved forms to seprate components

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,34 +1,62 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Footer from "../components/Footer/Footer";
+import LogInForm from "../components/LogInForm/LogInForm";
 import "../styles/home.css";
+import API from "../utils/API";
 
-export default function Home() {
+class Home extends Component {
+    state = {
+        email: "",
+        password: "",
+        userInfo: {}
+    };
+
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+
+    onSubmit = event => {
+        event.preventDefault();
+        if (this.state.email === "" || this.state.password === "") {
+            return;
+        }
+        this.userInfo = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        // console.log(this.userInfo);
+        // return this.userInfo;
+        API.logIn(this.state.userInfo)
+        .then(function() {
+            window.location.replace("/user")
+        })
+        .catch((err) => console.log(err))
+
+    };
+
+render() {
     return (
         <div>
             <div className="basics">
                 <h1>Welcome to Chicken Tinder - Or header here instead</h1>
                 <h2>Description Here</h2>
                 <h3>Please log in or <a href="/signup">sign up</a></h3>
-                <form>
-                    <input
-                        value=""
-                        name=""
-                        onChange="" 
-                        type="email" 
-                        placeholder="Email" />
-                        <br />
-                    <input
-                        value=""
-                        name=""
-                        onChange="" 
-                        type="password" 
-                        placeholder="Password" />
-                        <br />
-                    <button onClick="">Log in</button>
-                </form>
+                <LogInForm
+                    email={this.state.email} 
+                    password={this.state.password}
+                    handleInputChange={this.handleInputChange}
+                    onSubmit={this.onSubmit}
+                />
                 <br /><a href="/user">Temp link to user</a>
             </div>
             <Footer />
         </div>
     )
 }
+    
+}
+
+export default Home;
