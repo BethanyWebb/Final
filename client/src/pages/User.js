020bf59f447d1e95f9587d51ef46ca1ae04ed804
Josh Brooks <defaultjosh@hotmail.com>
2020-07-25 11:49:45 -0400

    The user name displays from the token info

--- client/src/pages/User.js
+++ client/src/pages/User.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Footer from "../components/Footer/Footer";
 import "../styles/user.css";
 import { Form, FormGroup, Label, Input } from 'reactstrap';
+import API from '../utils/API';
 
 
 // Module for decoding json web tokens
@@ -10,22 +11,48 @@ const jwtDecode = require('jwt-decode');
 // This is waiting to have a value from getToken()
 let userToken;
 
+let userTokenName;
+
+// This is waiting on a value from the API get call
+
+
 // Looks for a token, if found it trims it to the exact value needed
 // for jwt-decode. Then it returns the decoded value as userToken
 const getToken = function() {
     userToken = window.localStorage.getItem('jwt')
     userToken = userToken.replace(/Bearer token: /, '')
-    console.log(userToken)
+    // console.log(userToken)
     const decoded = jwtDecode(userToken);
-    console.log(decoded);
-    console.log(decoded.name);
-    userToken = decoded.name;
+    // console.log(decoded);
+    // console.log(decoded.id);
+    userToken = decoded.id;
     return userToken
 };
 
+const getTokenName = function() {
+    userTokenName = window.localStorage.getItem('jwt')
+    userTokenName = userTokenName.replace(/Bearer token: /, '')
+    const decoded = jwtDecode(userTokenName);
+    userTokenName = decoded.name;
+    return userTokenName
+};
+
+// API call made using the id from the token to get the user's info
+const getUserInfo = function(userToken) {
+    API.getInfo(userToken)
+    .then(infoObj => console.log(infoObj.data))
+    .catch((err) => console.log(err))
+}
+
+// Get token id val and pass it in to api call
+const wholeUserInfo = function() {
+    getToken();
+    getUserInfo(userToken);
+    getTokenName();
+}
+
 // removes the web token and sends the user back to the home page
 const logOut = function(){
-    // this.clearLocal('jwt')
     window.localStorage.removeItem('jwt')
     window.location.replace("/")
 }
@@ -39,10 +66,11 @@ const noLogin = () => {
 // from a successful logon. Passes function to the button.
 export default function User() {
     if(localStorage.getItem('jwt')) {
+wholeUserInfo();
     return (
         <div>
             <div className="basics">
-                <h1>Hello "username here"</h1>
+    <h1>Hello {userTokenName}!</h1>
                 <p>A few questions and your dining dilema will be fixed</p>
                 <Form>
                     <div className="divSpace">
@@ -71,7 +99,7 @@ export default function User() {
                     </div>
                 </Form>
                 <button onClick={logOut}>Sign Out</button>
-                <button onClick={getToken}>Test Button</button>
+                <button onClick={wholeUserInfo}>Test Button</button>
                 <br /><a href="/">Temp link to home</a>
             </div>
             <Footer />
