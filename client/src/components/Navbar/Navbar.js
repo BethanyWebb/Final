75b7d9a3e80c58ecffe2cd8c2b2cffdce656db05
brandon chontos <brandonchontos@gmail.com>
2020-07-28 21:17:39 -0400

    various fixes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -9,15 +9,15 @@ function MyNavbar() {
                 <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
                     <ReactBootStrap.Nav className="mr-auto">
-                    <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
+                        <ReactBootStrap.Nav.Link href="/">Home</ReactBootStrap.Nav.Link>
                         <ReactBootStrap.Nav.Link href="/signup">Sign Up</ReactBootStrap.Nav.Link>
                         <ReactBootStrap.Nav.Link href="/user">Your Page</ReactBootStrap.Nav.Link>
                         <ReactBootStrap.NavDropdown title="About Us" id="collasible-nav-dropdown">
-                            <ReactBootStrap.NavDropdown.Item href="#action/3.1" target="_blank">Stephen Settle</ReactBootStrap.NavDropdown.Item>
+                            <ReactBootStrap.NavDropdown.Item href="https://linkedin.com/in/stephen-settle" target="_blank">Stephen Settle</ReactBootStrap.NavDropdown.Item>
                             <ReactBootStrap.NavDropdown.Divider />
                             <ReactBootStrap.NavDropdown.Item href="https://reactportfolio14.herokuapp.com/" target="_blank">Bethany Webb</ReactBootStrap.NavDropdown.Item>
                             <ReactBootStrap.NavDropdown.Divider />
-                            <ReactBootStrap.NavDropdown.Item href="#action/3.3" target="_blank">Brandon Chontos</ReactBootStrap.NavDropdown.Item>
+                            <ReactBootStrap.NavDropdown.Item href="https://www.linkedin.com/in/brandon-chontos-18a1501b0/" target="_blank">Brandon Chontos</ReactBootStrap.NavDropdown.Item>
                             <ReactBootStrap.NavDropdown.Divider />
                             <ReactBootStrap.NavDropdown.Item href="https://joshua-brooks-portfolio.herokuapp.com/" target="_blank">Joshua Brooks</ReactBootStrap.NavDropdown.Item>
                             <ReactBootStrap.NavDropdown.Divider />
